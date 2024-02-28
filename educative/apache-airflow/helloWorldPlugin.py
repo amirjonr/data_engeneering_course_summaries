a438610210a0c6f16109dcbc04510a299dee2c1e
amirjonr <amirshorakhimov0017@gmail.com>
2024-02-28 15:22:03 +0500

    added gitignore file

--- educative/apache-airflow/helloWorldPlugin.py
+++
@@ -1,8 +0,0 @@
-from airflow.plugins_manager import AirflowPlugin
-
-from createAnOperator import HelloWorldOperator
-
-
-class HelloWorldPlugin(AirflowPlugin):
-    name = 'hello_world_plugin'
-    operators = [HelloWorldOperator]
