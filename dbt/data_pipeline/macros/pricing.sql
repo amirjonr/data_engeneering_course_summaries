fe65580782bb8a615fcc6ca28cb9e007eaaecec3
amirjonr <amirshorakhimov0017@gmail.com>
2024-04-18 11:28:52 +0500

    fix

--- dbt/data_pipeline/macros/pricing.sql
+++ dbt/data_pipeline/macros/pricing.sql
@@ -1,3 +1,3 @@
-{% macro discounted_amount(extended_price, discount_persentage, scale=2) %}
+{% macro discounted_amount(extended_price, discount_percentage, scale=2) %}
     (-1 * {{ extended_price }} {{ discount_persentage }})::decimal(16, {{ scale }})
 {% endmacro %}
